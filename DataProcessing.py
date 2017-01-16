49dd901c4f085db4b4119d2cbf4ee174eb06d3b3
petritn <petrit.nahi@gmail.com>
2017-01-16 19:59:51 +0100

    Added data processing examples file.

--- DataProcessing.py
+++ DataProcessing.py
@@ -1,4 +1,12 @@
 import pandas as pd
+import numpy as np
+
 df = pd.DataFrame([ ['green','M','10.1', 'class1'], ['red', 'L', '13.5', 'class2'], ['blue', 'XL', '15.3', 'class1']])
 df.columns=['color', 'size','price','class-label']
-df
+print(df)
+
+class_mapping={label:idx for idx, label in enumerate(np.unique(df['class-label']))}
+print(class_mapping)
+
+df['class-label']=df['class-label'].map(class_mapping)
+print(df)
