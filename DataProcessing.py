1bf07c181718fa1c73a492ca8da33dba84dc21cd
petritn <petrit.nahi@gmail.com>
2017-01-20 22:56:44 +0100

    Completed exercises on data preprocessing including dimensionality reduction with unsupervised Principal Component Analysis (PCA) and supervised Linear Discriminant Analysis (LDA).

--- DataProcessing.py
+++ DataProcessing.py
@@ -1,5 +1,15 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.cross_validation import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
+from Preprocessing.CSBS import CSBS
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.decomposition import PCA
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from Functions import plot_decision_regions
 
 df = pd.DataFrame([ ['green','M','10.1', 'class1'], ['red', 'L', '13.5', 'class2'], ['blue', 'XL', '15.3', 'class1']])
 df.columns=['color', 'size','price','class-label']
@@ -8,12 +18,173 @@ print(df)
 class_mapping={label:idx for idx, label in enumerate(np.unique(df['class-label']))}
 print(class_mapping)
 
-df['class-label']=df['class-label'].map(class_mapping)
-print(df)
+#df['class-label']=df['class-label'].map(class_mapping)
+#print(df)
 
-df_wine = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-database/wine/wine.data')
-df_wine.columns = ['Class Label', 'Alcohol', 'Malic Acid', 'Ash', 'Alcalinity of Ash', 'Magnesium', 'Total Phenols',
-                   'Flavanoids', 'Non-Flavanoid Phenols', 'Proanthocyannins', 'Color Intensity', 'Hue',
-                   'OD280/OD315 of diluted wines', 'Proline']
+df_wine = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/wine/wine.data')
+df_wine.columns = ['Class Label', 'Alcohol',
+                   'Malic Acid', 'Ash',
+                   'Alcalinity of Ash', 'Magnesium',
+                   'Total Phenols', 'Flavanoids',
+                   'Non-Flavanoid Phenols',
+                   'Proanthocyanins',
+                   'Color Intensity', 'Hue',
+                   'OD280/OD315 of diluted wines',
+                   'Proline']
 print('Class labels', np.unique(df_wine['Class Label']))
 df_wine.head()
+
+X, y = df_wine.iloc[:, 1:].values, df_wine.iloc[:, 0].values
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
+
+stdsc = StandardScaler()
+X_train_std = stdsc.fit_transform(X_train)
+X_test_std = stdsc.fit_transform(X_test)
+
+fig = plt.figure()
+ax = plt.subplot(1, 1, 1)
+
+colors = ['blue', 'green', 'red', 'cyan', 'magenta', 'yellow', 'black', 'pink', 'lightgreen', 'lightblue',
+          'gray', 'indigo', 'orange']
+
+weights, params = [], []
+for c in np.arange(-4, 6):
+    lr = LogisticRegression(penalty='l1', C=10 ** c, random_state=0)
+    lr.fit(X_train_std, y_train)
+    weights.append(lr.coef_[1])
+    params.append(10 ** c)
+
+weights = np.array(weights)
+
+for column, color in zip(range(weights.shape[1]), colors):
+    plt.plot(params, weights[:, column],
+             label=df_wine.columns[column + 1],
+             color=color)
+plt.axhline(0, color='black', linestyle='--', linewidth=3)
+plt.xlim([10 ** (-5), 10 ** 5])
+plt.ylabel('weight coefficient')
+plt.xlabel('C')
+plt.xscale('log')
+plt.grid()
+plt.legend(loc='upper right', bbox_to_anchor=(1.38, 1.03),
+           ncol=1, shadow=True, fancybox=True)
+plt.show()
+
+################
+# Demonstrating Sequential Backward Selection (SBS) with k-nearest neighbor classifier
+
+knn = KNeighborsClassifier(n_neighbors=2)
+sbs = CSBS(knn, k_features=1)
+sbs.fit(X_train_std, y_train)
+
+k_feat = [len(k) for k in sbs.subsets_]
+plt.plot(k_feat, sbs.scores_, marker='o')
+plt.ylim([0.7, 1.1])
+plt.ylabel('Accuracy')
+plt.xlabel('Number of features')
+plt.grid()
+plt.show()
+
+k5 = list(sbs.subsets_[6])
+print('Selected feature subset:', df_wine.columns[1:][k5])
+
+knn.fit(X_train_std, y_train)
+print('Training Accuracy:', knn.score(X_train_std, y_train))
+print('Test Accuracy', knn.score(X_test_std, y_test))
+
+# Now using the k5 feature subset to see how KNN performs
+
+knn.fit(X_train_std[:, k5], y_train)
+print('Training Accuracy (using subset):', knn.score(X_train_std[:, k5], y_train))
+print('Test Accuracy (using subset):', knn.score(X_test_std[:, k5], y_test))
+
+"""
+# Random forests for feature importance assessment
+
+feat_labels = df_wine.columns[1:]
+
+forest = RandomForestClassifier(n_estimators=10000,
+                                random_state=0,
+                                n_jobs=-1)
+
+forest.fit(X_train, y_train)
+importances = forest.feature_importances_
+
+indices = np.argsort(importances)[::-1]
+
+for f in range(X_train.shape[1]):
+    print("%2d) %-*s %f" % (f + 1, 30,
+                            feat_labels[indices[f]],
+                            importances[indices[f]]))
+
+plt.title('Feature Importances')
+plt.bar(range(X_train.shape[1]),
+        importances[indices],
+        color='lightblue',
+        align='center')
+
+plt.xticks(range(X_train.shape[1]),
+           feat_labels[indices], rotation=90)
+plt.xlim([-1, X_train.shape[1]])
+plt.tight_layout()
+#plt.savefig('./random_forest.png', dpi=300)
+plt.show()
+"""
+
+######
+# Unsupervised dimensionality reduction using principle component analysis (PCA)
+
+cov_mat = np.cov(X_train_std.T)
+eigen_vals, eigen_vecs = np.linalg.eig(cov_mat)
+print('\nEigenvalues \n%s' %eigen_vals)
+
+tot = sum(eigen_vals)
+var_exp = [(i / tot) for i in sorted(eigen_vals, reverse = True)]
+cum_var_exp = np.cumsum(var_exp)
+
+plt.bar(range(1,14), var_exp, alpha=0.5, align='center', label='individual explained variance')
+plt.step(range(1,14), cum_var_exp, where='mid', label='cumulative explained variance')
+plt.ylabel('Explained variance ratio')
+plt.xlabel('Principal Components')
+plt.legend(loc='best')
+plt.show()
+
+pca = PCA(n_components=2)
+lr = LogisticRegression()
+X_train_pca = pca.fit_transform(X_train_std)
+X_test_pca = pca.transform(X_test_std)
+lr.fit(X_train_pca, y_train)
+plot_decision_regions(X_train_pca, y_train, classifier=lr)
+plt.xlabel('PC1')
+plt.ylabel('PC2')
+plt.legend(loc='best')
+plt.show()
+
+plot_decision_regions(X_test_pca, y_test, classifier=lr)
+plt.xlabel('PC1')
+plt.ylabel('PC2')
+plt.legend(loc='best')
+plt.show()
+
+######
+# Supervised data compression with Linear Discriminant Analysis (LDA)
+
+lda = LinearDiscriminantAnalysis(n_components=2)
+X_train_lda = lda.fit_transform(X_train_std, y_train)
+X_test_lda = lda.transform(X_test_std)
+lr = lr.fit(X_train_lda, y_train)
+
+plot_decision_regions(X_train_lda, y_train, classifier=lr)
+plt.xlabel('LD1')
+plt.ylabel('LD2')
+plt.legend(loc='best')
+plt.title('Training Data')
+plt.show()
+
+plot_decision_regions(X_test_lda, y_test, classifier=lr)
+plt.xlabel('LD1')
+plt.ylabel('LD2')
+plt.legend(loc='best')
+plt.title('Test Data')
+plt.show()
+
